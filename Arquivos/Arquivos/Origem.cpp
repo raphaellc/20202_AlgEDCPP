bf79c0b41d221a96c82c35c3cda0ae288a33910e
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2020-11-05 21:34:27 -0300

    arquivos funcionando

--- Arquivos/Arquivos/Origem.cpp
+++ Arquivos/Arquivos/Origem.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include "Unidade.h"
-
+#include <string>
 using namespace std;
 
 int main ()
@@ -13,7 +13,7 @@ int main ()
 
 	Unidade * un5 = new Unidade[4];
 
-	fstream arq("teste.bin", ios::binary | ios::out);
+	ofstream arq("teste.bin", ios::binary);
 	
 	if(!arq.is_open())
 	{
@@ -29,7 +29,7 @@ int main ()
 		arq.close();
 	}	
 
-	ifstream arqr("teste.bin", ios::binary);
+	ifstream arqr("teste.bin", ifstream::binary);
 	if(!arqr.is_open())
 	{
 		cout << "O arquivo não abriu\n";
@@ -46,16 +46,92 @@ int main ()
 
      for (int i = 0; i < 4; i++)
 	{
-
-	
-	cout<<"UNIDADE "+to_string(i+1)<<":"<<endl;
+	cout<<"UNIDADE"+to_string(i + 1)<<":"<<endl;
 	cout<<"ID: " << un5[i].id<<endl;
 	cout <<"Defesa: " << un5[i].defesa << endl;
 	cout<<"Ataque: "<< un5[i].ataque<<endl;
 	}
-	//a gnt vai imprimir por um ofstream ou um f?
+	
+	 Unidade * aux = new Unidade;
+	 arqr.open("teste.bin", ifstream::binary);
+	 if (!arqr.is_open())
+	 {
+		 cout << "O arquivo não abriu\n";
+	 }
+	 else
+	 {
+		 arqr.seekg(2 * sizeof(Unidade));
+		 
+	 	arqr.read(reinterpret_cast<char*>(aux), sizeof(Unidade));
+
+		 arqr.close();
+	 }
+
+	 cout << "UNIDADE aux " + to_string(3) << ":" << endl;
+	 cout << "ID: " << aux->id << endl;
+	 cout << "Defesa: " << aux->defesa << endl;
+	 cout << "Ataque: " << aux->ataque << endl;
+
+	 ofstream oarq("teste.bin", ofstream::binary | ios::in);
+	if (!oarq.is_open())
+	{
+			cout << "O arquivo não abriu\n";
+	}
+	else
+	{
+		oarq.seekp(2*sizeof(Unidade),ios_base::beg);
+		oarq.write(reinterpret_cast<const char *>(aux), sizeof(Unidade));
+	}
+	oarq.close();
 
 
+	/*
+	arqr.open("teste.bin", ifstream::binary);
+	if (!arqr.is_open())
+	{
+		cout << "O arquivo não abriu\n";
+	}
+	else
+	{
+		arqr.seekg(ios::beg);
+
+		arqr.read(reinterpret_cast<char*>(aux), sizeof(Unidade));
+
+		arqr.close();
+	}
+	cout << "==========================================" << endl;
+	cout << "UNIDADE aux " + to_string(0) << ":" << endl;
+	cout << "ID: " << aux->id << endl;
+	cout << "Defesa: " << aux->defesa << endl;
+	cout << "Ataque: " << aux->ataque << endl;*/
+	
+
+	arqr.open("teste.bin", ios::binary);
+	if (!arqr.is_open())
+	{
+		cout << "O arquivo não abriu\n";
+	}
+	else
+	{
+		arqr.seekg(ios::beg);
+		int i = 0;
+		while (!arqr.eof()) {
+			arqr.read(reinterpret_cast<char*>(&un5[i]), sizeof(Unidade));
+			i++;
+		}
+
+		arqr.close();
+	}
+
+	cout << "===========================================" << endl;
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "UNIDADE" + to_string(i + 1) << ":" << endl;
+		cout << "ID: " << un5[i].id << endl;
+		cout << "Defesa: " << un5[i].defesa << endl;
+		cout << "Ataque: " << un5[i].ataque << endl;
+	}
+	
 	system ("pause");
 	return 0;
 }
