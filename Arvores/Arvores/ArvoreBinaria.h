73920984f909a2f701fb44b06e3c81dc7e2a290d
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2020-11-10 20:16:41 -0300

    No v1

--- Arvores/Arvores/ArvoreBinaria.h
+++ Arvores/Arvores/ArvoreBinaria.h
@@ -1,5 +1,27 @@
+#include "No.h"
 template <class T>
 class ArvoreBinaria
 {
-	
+public:
+	ArvoreBinaria();
+	~ArvoreBinaria();
+	void inserir(T valor);
+	No<T>* buscar(T valor);
+	void deletar(T valor);
+	void visitarPreOrdem();
+	void visitarCentral();
+	void visitarPosOrdem();
+	void visitarLargura();
+	int altura(No<T> *nodo);
+	No<T>* getRaiz();
+private:
+	No<T> *raiz;
+	void inserir(T valor, No<T>* raiz);
+	No<T>* buscar(T valor, No<T>* raiz);
+	void deletar(T valor, No<T>* raiz);
+	void visitarPreOrdem(No<T>* n);
+	void visitarCentral(No<T>* n);
+	void visitarPosOrdem(No<T>* n);
+	void visitarLargura(No<T>* n);
+
 };
--- Arvores/Arvores/No.h
+++ Arvores/Arvores/No.h
@@ -5,32 +5,61 @@ class No
 {
 public:
 	No();
-	~No();
+	~No() {};
 
 	T getDado();
 	void setDado(T _dado);
 
-	No* getEsq();
-	No* getDir();
+	No<T>* getEsq();
+	No<T>* getDir();
 
 	void setEsq(No* _noEsq);
 	void setDir(No* _noDir);
 
-	
-
 private:
 	T dado;
-	No* esq, dir;
-
+	No<T>* esq;
+	No<T>* dir;
 };
 
-
-template<T>
-No::T getDado()
+template <class T>
+No<T>::No()
 {
+	esq = nullptr; dir = nullptr;		
+}
 
+template <class T>
+T No<T>::getDado()
+{
+	return dado;
 } 
+template <class T>
+void No<T>::setDado(T _dado)
+{
+	dado = _dado;
+}
 
-template<T>
-No::void setDado(T _dado)
+template <class T>
+No<T>* No<T>::getEsq()
+{
+	return esq;
+}
 
+template <class T>
+No<T>* No<T>::getDir()
+{
+	return dir;
+}
+
+template <class T>
+void  No<T>::setEsq(No<T>* _noEsq)
+{
+	esq = _noEsq;
+}
+	
+template <class T>
+void No<T>::setDir(No<T>* _noDir)
+{
+	dir = _noDir;
+}
+	
