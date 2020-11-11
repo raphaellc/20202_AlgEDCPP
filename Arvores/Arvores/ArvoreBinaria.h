564969b27d6915c9f11d2f6de6b686f902a0e7d1
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2020-11-10 21:52:59 -0300

    Correcao

--- Arvores/Arvores/ArvoreBinaria.h
+++ Arvores/Arvores/ArvoreBinaria.h
@@ -1,8 +1,7 @@
 #pragma once
 #include <iostream>
-
-
 #include "No.h"
+
 template <class T>
 class ArvoreBinaria
 {
---
+++ Arvores/Arvores/No.h
@@ -0,0 +1,63 @@
+#pragma once
+template <class T>
+class No
+{
+public:
+	No();
+	~No() {};
+
+	T getDado();
+	void setDado(T _dado);
+
+	No<T>* getEsq();
+	No<T>* getDir();
+
+	void setEsq(No* _noEsq);
+	void setDir(No* _noDir);
+
+private:
+	T dado;
+	No<T>* esq;
+	No<T>* dir;
+};
+
+template <class T>
+No<T>::No()
+{
+	esq = nullptr; dir = nullptr;
+}
+
+template <class T>
+T No<T>::getDado()
+{
+	return dado;
+}
+template <class T>
+void No<T>::setDado(T _dado)
+{
+	dado = _dado;
+}
+
+template <class T>
+No<T>* No<T>::getEsq()
+{
+	return esq;
+}
+
+template <class T>
+No<T>* No<T>::getDir()
+{
+	return dir;
+}
+
+template <class T>
+void  No<T>::setEsq(No<T>* _noEsq)
+{
+	esq = _noEsq;
+}
+
+template <class T>
+void No<T>::setDir(No<T>* _noDir)
+{
+	dir = _noDir;
+}
