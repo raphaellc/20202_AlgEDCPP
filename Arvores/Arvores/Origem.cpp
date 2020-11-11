96788881bbf5500af1963eddd04e2852512dc3be
RAPHAEL LEITE CAMPOS <raphaellc@users.noreply.github.com>
2020-11-10 21:46:14 -0300

    Update Origem.cpp

---
+++ Arvores/Arvores/Origem.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <ctime>
+#include "ArvoreBinaria.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ptb");
+	ArvoreBinaria<int> * ab = new ArvoreBinaria<int>;
+	ab->inserir(10);
+	ab->inserir(20);
+	ab->inserir(5);
+	ab->inserir(3);
+	ab->visitarPreOrdem();
+
+	system("pause");
+	return 0;
+}
